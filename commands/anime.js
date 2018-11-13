d256c6bc8db6a098bd2ac5bf8197037b4202ba90
Vezqi <rmtlobbys@icloud.com>
2018-11-12 17:44:44 -0800

    Add files via upload

---
+++ commands/anime.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'anime',
+    description: 'Returns info about a given show from MyAnimeList',
+    args: true,
+    needsArgs: true,
+    usage: 'anime <query>',
+    execute(message, args) {
+        const query = args.join(' ');
+        const url = `https://api.jikan.moe/v3/search/anime?q=${encodeURIComponent(query)}` 
+        request(url, (error, response, data) => {
+            const result = JSON.parse(data).results[0];
+            const numFormat = num => num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            const startDate = new Date(result.start_date).toLocaleDateString();
+            const endDate = new Date(result.end_date).toLocaleDateString();
+            const color = result.airing ? 0x00FF04 : 0xFF0000;
+
+            const embed = new Discord.RichEmbed()
+                .setTitle(result.title)
+                .setURL(result.url)
+                .setDescription(result.synopsis)
+                .addField('Episodes', result.episodes)
+                .addField('Rating', result.rated)
+                .addField('Aired From', `${startDate} - ${endDate}`)
+                .setFooter(`Members: ${numFormat(result.members)}・Score: ${result.score}`)
+                .setThumbnail(result.image_url)
+                .setColor(color)
+
+            message.channel.send(embed);
+        });
+    }
+}
